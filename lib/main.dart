767162e802403a56e768f9f9efc0170085245772
Dharmin8 <ddmac2421@gmail.com>
2019-12-23 16:13:09 +0530

    Changed Titile to Nature Drive

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Social-App",
+      title: "Nature Drive",
       theme: new ThemeData(
         primaryColor: Colors.green,
         accentColor: new Color(0xffffffff),
