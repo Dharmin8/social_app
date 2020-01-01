40551023f0bb46294dbfa80ec13fda2c79491265
probe2k <kyash054@gmail.com>
2020-01-02 01:29:54 +0530

    Fixed tabsRouting - stream open for MaterialPageRouting

--- lib/app_home.dart
+++ lib/app_home.dart
@@ -11,117 +11,103 @@ class AppHome extends StatefulWidget {
 }
 
 class _AppHomeState extends State<AppHome> with SingleTickerProviderStateMixin {
-int _currentIndex = 0;
-void onTabTapped (int index){
-  setState(() {
-    _currentIndex= index;
-  });
-}
- final List<Widget> children = [
-  new Home(),
-  new Search(),
-  new Favorite(),
-  new Check(),
-  new Person(),
+  int _currentIndex = 0;
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
-];
-
+  final List<Widget> tabs = [
+    new Home(),
+    new Search(),
+    new Favorite(),
+    new Check(),
+    new Person(),
+  ];
 
   @override
-
   Widget build(BuildContext context) {
-
     return new Scaffold(
-        appBar: new AppBar(
-          centerTitle: true,
-          title: new Text("Nature Drive"),
-          elevation: 0.7,
-
-          bottom:
-          PreferredSize(
-            preferredSize: Size(50, 150),
-            child: Container(
-              alignment: Alignment.center,
-              child: RaisedButton(
-                  textColor: Colors.white,
-                  color: Colors.green,
-                  child: Text("Mumbai"),
-                  elevation: 0,
-                  onPressed: () {}),
-            ),
+      appBar: new AppBar(
+        centerTitle: true,
+        title: new Text("Nature Drive"),
+        elevation: 0.7,
+        bottom: PreferredSize(
+          preferredSize: Size(50, 150),
+          child: Container(
+            alignment: Alignment.center,
+            child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text("Mumbai"),
+                elevation: 0,
+                onPressed: () {}),
           ),
-//
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: new Icon(Icons.notifications_none),
-            )
-          ],
         ),
-      body:
-
-      Scaffold(
-
-        bottomNavigationBar: BottomNavigationBar(
-
-          onTap: onTabTapped,
-          currentIndex: _currentIndex ,
-
-          type: BottomNavigationBarType.fixed,
-          iconSize: 25,
-
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.home),
-              title: new Text("Home"),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.search),
-              title: new Text("Search"),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.favorite),
-              title: new Text("Marked"),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.check),
-              title: new Text("Attended"),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.person),
-              title: new Text("Profile"),
-            ),
-          ],
-
-        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: new Icon(Icons.notifications_none),
+          )
+        ],
       ),
-        drawer: new Drawer(
-          child: ListView(
-            children: <Widget>[
-              new ListTile(
-                title: new Text(
-                  'Application Settings',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontSize: 30.0,
-                  ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 25,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.search),
+            title: new Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.favorite),
+            title: new Text("Marked"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.check),
+            title: new Text("Attended"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text("Profile"),
+          ),
+        ],
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new ListTile(
+              title: new Text(
+                'Application Settings',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 30.0,
                 ),
               ),
-              new ListTile(
-                title: new Text('Theme'),
-                onTap: (null),
-              ),
-              new ListTile(
-                title: new Text('Contact Us'),
-              ),
-              new ListTile(
-                title: new Text('Legal'),
-              ),
-            ],
-          ),
+            ),
+            new ListTile(
+              title: new Text('Theme'),
+              onTap: (null),
+            ),
+            new ListTile(
+              title: new Text('Contact Us'),
+            ),
+            new ListTile(
+              title: new Text('Legal'),
+            ),
+          ],
         ),
-        backgroundColor: new Color(0xffffffff),
-       );
+      ),
+      backgroundColor: new Color(0xffffffff),
+    );
   }
 }
